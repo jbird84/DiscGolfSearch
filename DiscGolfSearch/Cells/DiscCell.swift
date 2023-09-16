3ca211821111c49ee27306a5915cc2a6e9de49d9
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-09-15 19:01:17 -0500

    continue to work on dis app

---
+++ DiscGolfSearch/Cells/DiscCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiscCell.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 9/14/23.
+//
+
+import UIKit
+
+class DiscCell: UICollectionViewCell {
+    
+}
---
+++ DiscGolfSearch/Extensions/Data+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Data+Ext.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 9/14/23.
+//
+
+import Foundation
