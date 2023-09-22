a536e1d394e119f2ce1e11b4931db1ba570a852d
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-09-22 08:08:46 -0500

    continued editing the disc info button and view

---
+++ DiscGolfSearch/Views/DiscTypesView.swift
@@ -0,0 +1,20 @@
+//
+//  DiscTypesView.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 9/22/23.
+//
+
+import UIKit
+
+class DiscTypesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
