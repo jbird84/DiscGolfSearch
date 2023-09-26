720dc1d8acf9c8fc8e04167a85adaae3d1a2999b
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-09-25 19:01:30 -0500

    added a way to compare discs.

---
+++ DiscGolfSearch/Cells/SimilarDiscCell.swift
@@ -0,0 +1,8 @@
+//
+//  SimilarDiscCell.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 9/24/23.
+//
+
+import Foundation
---
+++ DiscGolfSearch/Views/SearchDiscViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchDiscViewController.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 9/24/23.
+//
+
+import UIKit
+
+class SearchDiscViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
