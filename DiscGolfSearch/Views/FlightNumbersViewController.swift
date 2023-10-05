8d1591121889b0499a4d509489dd86fc77b683f1
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-10-05 08:38:35 -0500

    Finished the Flight numbers screens

---
+++ DiscGolfSearch/Views/FlightNumbersViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FlightNumbersViewController.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 10/4/23.
+//
+
+import UIKit
+
+class FlightNumbersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
