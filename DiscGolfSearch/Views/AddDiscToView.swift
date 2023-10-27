8e15e3f247a73e08d3ba785f5a5881649fddbdea
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-10-27 06:52:33 -0500

    Adding the Add disc to cart/bag view

---
+++ DiscGolfSearch/Views/AddDiscToView.swift
@@ -0,0 +1,20 @@
+//
+//  AddDiscToView.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 10/27/23.
+//
+
+import UIKit
+
+class AddDiscToView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
