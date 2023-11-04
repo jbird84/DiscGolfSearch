0d7a407d6a2bbe5efe2320bc7454390dea86705b
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-11-03 19:10:27 -0500

    No new change

--- DiscGolfSearch/Views/AddDiscToView.swift
+++ DiscGolfSearch/Views/AddDiscToView.swift
@@ -17,11 +17,13 @@ class AddDiscToView: UIView {
     
     @IBAction func addToCartTapped(_ sender: Any) {
        
+        //Send user to fillout form then pop to view
         print("Added Disc To Cart!")
     }
     
     
     @IBAction func addToBagTapped(_ sender: Any) {
+        //Send user to fillout form then pop to view
         print("Added Disc To Bag!")
     }
     
