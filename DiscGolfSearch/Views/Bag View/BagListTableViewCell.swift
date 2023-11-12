efb92f2270f37dbf4645d9856beb91397a846180
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-11-12 14:46:53 -0600

    started working on the bag list

---
+++ DiscGolfSearch/Views/Bag View/BagListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BagListTableViewCell.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 11/12/23.
+//
+
+import UIKit
+
+class BagListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
