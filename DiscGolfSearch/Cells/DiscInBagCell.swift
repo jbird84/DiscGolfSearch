53fb83652a0ca87e8703ba28fa0759157c8e658c
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-11-26 08:42:10 -0600

    Worked on editing the discs in the bag

---
+++ DiscGolfSearch/Cells/DiscInBagCell.swift
@@ -0,0 +1,23 @@
+//
+//  DiscInBagCell.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 11/25/23.
+//
+
+import UIKit
+
+class DiscInBagCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
