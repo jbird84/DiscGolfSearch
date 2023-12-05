6b19a98b00844285d1cf9d5dbc9fe38bc2951fbc
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-12-05 07:07:20 -0600

    implementing core data with the Bag

---
+++ DiscGolfSearch/Models/BagDataModel.swift
@@ -0,0 +1,25 @@
+//
+//  Bag.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 11/12/23.
+//
+
+import UIKit
+import SwiftData
+
+struct BagDataModel {
+    let id: Int
+    let bagHexColor: String
+    let bagTitle: String
+    let bagType: String
+    let discs: [DiscGolfDisc]
+    
+    init(id: Int, bagHexColor: String, bagTitle: String, bagType: String) {
+        self.id = id
+        self.bagHexColor = bagHexColor
+        self.bagTitle = bagTitle
+        self.bagType = bagType
+        self.discs = []
+    }
+}
