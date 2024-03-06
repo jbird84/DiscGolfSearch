210bcb66619df66941c3402c18358e10e2a8614d
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-03-05 19:27:54 -0600

    started working on the OneDimensionalBar chart

---
+++ DiscGolfSearch/Helpers/Charts/ChartsDataModels/ChartData.swift
@@ -0,0 +1,8 @@
+//
+//  ChartData.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 3/5/24.
+//
+
+import Foundation
---
+++ DiscGolfSearch/Helpers/Charts/OneDimensionalBar.swift
@@ -0,0 +1,18 @@
+//
+//  OneDimensionalBar.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 3/5/24.
+//
+
+import SwiftUI
+
+struct OneDimensionalBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OneDimensionalBar()
+}
