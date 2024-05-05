3271fc35bc9e3437a8bb53aac9137b04b2c709ca
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-05-05 08:14:22 -0500

    fixed cart view image not appearing

--- DiscGolfSearch/Views/CartView/CartViewController.swift
+++ DiscGolfSearch/Views/CartView/CartViewController.swift
@@ -108,11 +108,7 @@ extension CartViewController: DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
     
     func imageTintColor(forEmptyDataSet scrollView: UIScrollView) -> UIColor? {
         let userInterfaceStyle = scrollView.traitCollection.userInterfaceStyle
-        if userInterfaceStyle != .dark {
-            return UIColor.label
-        } else {
-            return UIColor.secondaryLabel
-        }
+        return UIColor.white
     }
     
     func description(forEmptyDataSet scrollView: UIScrollView) -> NSAttributedString? {
