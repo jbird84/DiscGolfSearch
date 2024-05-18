36d7b91326cc0de17981a2b338619102ea48f6b1
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-05-18 07:49:07 -0500

    continue laying out this help view

---
+++ DiscGolfSearch/Views/Help View/ChooseHelpTypeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChooseHelpTypeViewController.swift
+//  DiscGolfSearch
+//
+//  Created by Kinney Kare on 5/18/24.
+//
+
+import UIKit
+
+class ChooseHelpTypeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
