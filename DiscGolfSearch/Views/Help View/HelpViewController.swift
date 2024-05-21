39844a50cce2330bc49e3d36df135e274fc49224
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-05-20 19:29:30 -0500

    Continued building out the form for support view

--- DiscGolfSearch/Views/Help View/HelpViewController.swift
+++ DiscGolfSearch/Views/Help View/HelpViewController.swift
@@ -21,13 +21,40 @@ class HelpViewController: FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       createContactSupportForm()
     }
     
-
+    private func setupView() {
+        // Add a "SEND" button to the navigation bar
+        let saveButton = UIBarButtonItem(title: "SEND", style: .plain, target: self, action: #selector(sendButtonTapped))
+        navigationItem.rightBarButtonItem = saveButton
+    }
     private func createContactSupportForm() {
         form
         +++ Section("Your Name")
+        <<< LabelRow(CellTags.customerName.rawValue) {
+            $0.title =  "Name"
+        }
+        <<< EmailRow(CellTags.customerEmail.rawValue) {
+            $0.title =  "Email"
+        }
+        <<< NumericRow(CellTags.customerPhone.rawValue) {
+            $0.title =  "Number"
+        }
+        <<< LabelRow(CellTags.customerSubject.rawValue) {
+            $0.title =  "Subject"
+        }
+        <<< TextRow(CellTags.customerMessage.rawValue) {
+            $0.title =  "Message"
+        }
+    }
+    
+    @objc private func saveButtonTapped() {
         
+       
+            navigationController?.popViewController(animated: true)
+            AlertPresenter.instance.showAlert(title: "Success", body: "Email Sent To Support Successfully!", iconImage: UIImage(systemName: "checkmark.circle.fill")!, bannerColor: .green) {
+            }
+        }
     }
 }
