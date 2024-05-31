1cef17f098be3ccb503b63f1f6f847f7b632d41c
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-05-30 22:05:10 -0500

    Cleanup view

--- DiscGolfSearch/Views/Help View/HelpViewController.swift
+++ DiscGolfSearch/Views/Help View/HelpViewController.swift
@@ -28,7 +28,7 @@ class HelpViewController: FormViewController {
     
     private func setupView() {
         title = "Contact Support"
-        // Add a "SEND" button to the navigation bar
+        
         let saveButton = UIBarButtonItem(title: "SEND", style: .plain, target: self, action: #selector(sendButtonTapped))
         navigationItem.rightBarButtonItem = saveButton
     }
@@ -58,42 +58,33 @@ class HelpViewController: FormViewController {
     }
     
     @objc private func sendButtonTapped() {
-        // Validate required fields
-                let nameRow: TextRow? = form.rowBy(tag: CellTags.customerName.rawValue)
-                let emailRow: EmailRow? = form.rowBy(tag: CellTags.customerEmail.rawValue)
-                let phoneRow: PhoneRow? = form.rowBy(tag: CellTags.customerPhone.rawValue)
-                let subjectRow: TextRow? = form.rowBy(tag: CellTags.customerSubject.rawValue)
-                let messageRow: TextAreaRow? = form.rowBy(tag: CellTags.customerMessage.rawValue)
-
-                // Collect the values
-                let name = nameRow?.value
-                let email = emailRow?.value
-                let phone = phoneRow?.value  // Optional
-                let subject = subjectRow?.value
-                let message = messageRow?.value
-
-                // Check if any required field is empty
-                if name?.isEmpty ?? true || email?.isEmpty ?? true || subject?.isEmpty ?? true || message?.isEmpty ?? true {
-                    // Show alert if any required field is empty
-                    let alertController = UIAlertController(title: "Error", message: "Please fill in all required fields.", preferredStyle: .alert)
-                    alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                    present(alertController, animated: true, completion: nil)
-                } else {
-                    // Print the information if all required fields are filled
-                    
-                    sendEmail(email: email ?? "Email Not Provided", title: subject ?? "Subject Not Provided", name: name ?? "Name Not Provided", phone: phone ?? "Phone Not Provided", body: message ?? "Message Not Provided")
-                    print("Name: \(name ?? "")")
-                    print("Email: \(email ?? "")")
-                    print("Phone: \(phone ?? "N/A")")
-                    print("Subject: \(subject ?? "")")
-                    print("Message: \(message ?? "")")
-
-                    // Dismiss the view controller and show success alert
-                    navigationController?.popViewController(animated: true)
-                    AlertPresenter.instance.showAlert(title: "Success", body: "Email Sent To Support Successfully!", iconImage: UIImage(systemName: "checkmark.circle.fill")!, bannerColor: .green) { }
-                }
-            }
+        
+        let nameRow: TextRow? = form.rowBy(tag: CellTags.customerName.rawValue)
+        let emailRow: EmailRow? = form.rowBy(tag: CellTags.customerEmail.rawValue)
+        let phoneRow: PhoneRow? = form.rowBy(tag: CellTags.customerPhone.rawValue)
+        let subjectRow: TextRow? = form.rowBy(tag: CellTags.customerSubject.rawValue)
+        let messageRow: TextAreaRow? = form.rowBy(tag: CellTags.customerMessage.rawValue)
+        
+        let name = nameRow?.value
+        let email = emailRow?.value
+        let phone = phoneRow?.value
+        let subject = subjectRow?.value
+        let message = messageRow?.value
+        
+        if name?.isEmpty ?? true || email?.isEmpty ?? true || subject?.isEmpty ?? true || message?.isEmpty ?? true {
+            
+            let alertController = UIAlertController(title: "Error", message: "Please fill in all required fields.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        } else {
+            
+            sendEmail(email: email ?? "Email Not Provided", title: subject ?? "Subject Not Provided", name: name ?? "Name Not Provided", phone: phone ?? "Phone Not Provided", body: message ?? "Message Not Provided")
+            
+            navigationController?.popViewController(animated: true)
+            AlertPresenter.instance.showAlert(title: "Success", body: "Email Sent To Support Successfully!", iconImage: UIImage(systemName: "checkmark.circle.fill")!, bannerColor: .green) { }
         }
+    }
+}
 
 extension HelpViewController: MFMailComposeViewControllerDelegate {
     func sendEmail(email: String, title: String, name: String, phone: String, body: String) {
@@ -104,14 +95,14 @@ extension HelpViewController: MFMailComposeViewControllerDelegate {
             mail.setToRecipients(["joshkinney@kinneykare.com"])
             mail.setSubject(title)
             mail.setMessageBody("<p> Date: \(Date())</p> <br /> <p> \(title)</p> <br /> <p> \(name)</p> <br /> <p> \(phone)</p> <br /> <p> Body: \(body)</p>", isHTML: true)
-
+            
             present(mail, animated: true)
         } else {
             //TODO: show failure alert
             
         }
     }
-
+    
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true)
     }
