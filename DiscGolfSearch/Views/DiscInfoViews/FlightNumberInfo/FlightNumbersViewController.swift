f2c91e9926516bd27a24aad49b9a3e33f49fd536
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-10-28 07:02:18 -0500

    Fixed some info in the reference guide

--- DiscGolfSearch/Views/DiscInfoViews/FlightNumberInfo/FlightNumbersViewController.swift
+++ DiscGolfSearch/Views/DiscInfoViews/FlightNumberInfo/FlightNumbersViewController.swift
@@ -14,6 +14,10 @@ struct FlightCharacteristic {
 
 class FlightNumbersViewController: UIViewController {
     
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    
     
     @IBOutlet weak var flightNumberCategoryLabel: UILabel!
     @IBOutlet var descriptionLabels: [UILabel]?
@@ -21,31 +25,24 @@ class FlightNumbersViewController: UIViewController {
     
     private let flightCharacteristics: [FlightCharacteristic] = [
         FlightCharacteristic(category: "SPEED (1 TO 14)", descriptions: [
-            "Speed is like the blazing velocity of a superhero in flight.",
-            "Picture Speed 1 as a leisurely hero, strolling through the skies, while Speed 14 is the supersonic savior, streaking like lightning.",
-            "Speedy discs slice effortlessly through headwinds, but mastering their might requires finesse. On the other hand, gentler, slower discs don't mind a breeze; they're your steadfast allies in precision, especially when the wind has your back.",
-            "Novices, you'd be wise to wield the slower discs, for the swifter ones demand Herculean prowess.",
-            "So, choosing a disc is a bit like picking the right superhero for the job. Fast ones for long shots. It's all about having the perfect hero for each adventure on the disc golf course!"
+            "Speed tells you how fast a disc can travel through the air when you throw it.",
+            "Imagine throwing a ball. Some balls go really fast (like a baseball), while others go slower (like a soft, squishy ball). In disc golf, speed numbers usually range from 1 to 14. A disc with a speed of 5 is slower than one with a speed of 12.",
+            "If you want your disc to fly far, you need one with a higher speed. It’s like choosing between a regular car and a race car: the race car goes much faster!"
         ]),
         FlightCharacteristic(category: "GLIDE (1 TO 7)", descriptions: [
-            "Glide is akin to a disc's skyward stamina, reminiscent of an airborne behemoth.",
-            "Certain discs are soaring champions, keeping lofty aspirations alive (Glide 7), while others surrender to gravity's embrace with haste (Glide 1).",
-            "Fresh-faced adventurers adore high-glide discs; they deliver impressive distances with minimal exertion. Yet when tempests rage, low-glide discs stand their ground like unwavering titans.",
-            "So, choosing a disc is a bit like picking the right superhero for the job. Choose high glide for big distances. It's all about having the perfect hero for each adventure on the disc golf course!", ""
+            "Glide tells you how well a disc stays up in the air after you throw it.",
+            "Think about how some objects float easily, like a feather, while others fall quickly, like a rock. Glide numbers range from 1 to 7. A disc with a glide of 6 will stay in the air longer than one with a glide of 2.",
+            "If you want your disc to sail through the air smoothly and not drop quickly, you want a disc with good glide. It’s like a bird soaring on the wind—high glide helps the disc stay up longer!"
         ]),
         FlightCharacteristic(category: "TURN (+1 TO -5)", descriptions: [
-            "Turn dictates the disc's mid-air dance, deciding whether it leans left or right during its flight.",
-            "A disc donning a +1 rating boasts a straight-laced demeanor, refusing to waver from its path. Contrastingly, a -5 rating is a siren call to the rightward skies (for right-handed flyers), coaxing daring discs to dance on the ground.",
-            "Discs with -3 to -5 ratings are the acrobatic daredevils, tumbling and twirling.",
-            "Seeking precision amidst gusty winds? Opt for discs with conservative turns. But for the neophytes, discs with more turn are like a breeze to hurl.",
-            "So, choosing a disc is a bit like picking the right superhero for the job. Turn for curvy flights. It's all about having the perfect hero for each adventure on the disc golf course!"
+            "Turn shows how much the disc will curve to the right right after you throw it (if you’re throwing with your right hand).",
+            "Think of it like steering a bike. If you turn the handlebars, the bike goes in that direction. Turn numbers can be negative (like -3) or positive (like +1). A disc with a negative turn (like -2) will start turning to the right more than one with a 0 or positive turn.",
+            "If you want your disc to fly straight, you’ll want a lower turn number. If you want it to curve to the right, you’d pick a disc with a higher negative turn. It helps you decide how to throw and where you want the disc to go!"
         ]),
         FlightCharacteristic(category: "FADE (0 TO 5)", descriptions: [
-            "Fade mirrors a disc's show-stopping finale, akin to a superhero's grand exit.",
-            "A disc bearing a 0 rating executes a dignified, straight-backed finale, akin to a triumphant mission accomplished.",
-            "In contrast, a disc crowned with a 5 rating stages a spectacular, dramatic leftward curtain call (for right-handed performers).",
-            "High-fade discs are the virtuosos, saving their flair for dazzling feats and tricky maneuvers. These are your go-to heroes when the shot needs that extra pizzazz.",
-            "So, choosing a disc is a bit like picking the right superhero for the job. Fade for epic finishes. It's all about having the perfect hero for each adventure on the disc golf course!"
+            "Fade tells you how much the disc will hook to the left at the end of its flight.",
+            "Imagine if you threw a ball straight, but then it suddenly curved to the left before it landed. Fade numbers range from 0 to 5. A disc with a fade of 5 will hook left a lot, while one with a fade of 1 will hardly curve at all.",
+            "If you want your disc to go straight and then gently curve left at the end, you’d choose one with a little fade. If you want a sharp turn left, you’d pick one with a strong fade."
         ])
     ]
     
@@ -55,6 +52,13 @@ class FlightNumbersViewController: UIViewController {
     }
     
     private func updateFlightCharacteristics() {
+        view1.layer.cornerRadius = 10 // Set the corner radius
+        view1.layer.masksToBounds = true // Clip the content to the bounds of the view
+        view2.layer.cornerRadius = 10 // Set the corner radius
+        view2.layer.masksToBounds = true // Clip the content to the bounds of the view
+        view3.layer.cornerRadius = 10 // Set the corner radius
+        view3.layer.masksToBounds = true // Clip the content to the bounds of the view
+        
         guard let descriptionLabels = descriptionLabels else {
             return
         }
